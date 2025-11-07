69df9255d122b6f26c800778cc2955b9181409a7
jess <jr587atroblox@gmail.com>
2025-11-07 21:44:53 +0000

    version update + getPassMatrix (may not return good values)

--- common/src/main/java/soup587/figvra/mixin/RendererAPIMixin.java
+++ common/src/main/java/soup587/figvra/mixin/RendererAPIMixin.java
@@ -3,9 +3,12 @@ package soup587.figvra.mixin;
 import org.figuramc.figura.lua.LuaWhitelist;
 import org.figuramc.figura.lua.api.RendererAPI;
 import org.figuramc.figura.lua.docs.LuaMethodDoc;
+import org.figuramc.figura.math.matrix.FiguraMat4;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.vivecraft.api.client.VRRenderingAPI;
+import org.vivecraft.api.client.data.RenderPass;
 
 @Mixin(value = RendererAPI.class, remap = false)
 public abstract class RendererAPIMixin {
@@ -22,5 +25,18 @@ public abstract class RendererAPIMixin {
     @LuaMethodDoc("renderer.vr.is_first_pass")
     public boolean vrIsFirstPass() {
         return VRRenderingAPI.instance().isFirstRenderPass();
+    }
+
+    @Unique
+    @LuaWhitelist
+    @LuaMethodDoc("renderer.vr.get_pass_matrix")
+    public FiguraMat4 vrGetPassMatrix(String pass) {
+        Matrix4f mat = VRRenderingAPI.instance().getRenderPassMatrix(RenderPass.valueOf(pass.toUpperCase()));
+        return FiguraMat4.of(
+                mat.m00(), mat.m01(), mat.m02(), mat.m03(),
+                mat.m10(), mat.m11(), mat.m12(), mat.m13(),
+                mat.m20(), mat.m21(), mat.m22(), mat.m23(),
+                mat.m30(), mat.m31(), mat.m32(), mat.m33()
+        );
     }
 }
